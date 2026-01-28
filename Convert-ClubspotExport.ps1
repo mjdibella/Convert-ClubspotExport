c6240a1aeffa61a92c59b39dc8d701ea69073187
mjdibella <74016254+mjdibella@users.noreply.github.com>
2026-01-28 07:22:28 -0800

    Add files via upload

---
+++ Convert-ClubspotExport.ps1
@@ -0,0 +1,47 @@
+﻿param(
+    [Parameter(Mandatory=$true)][string]$importfile,
+    [Parameter()][switch]$skipperonly,
+    [Parameter()][switch]$unique
+)
+$entries = Import-csv $importfile
+$persons = @()
+foreach ($entry in $entries) {
+    $entry.Participants = $entry.Participants.Replace("<br><div class='table-row-tag grey' style='display:inline-block;'>Incomplete crew</div>", "")
+    $entry.Participants = $entry.Participants.Replace("<br>", ",")
+    $entry.Participants = $entry.Participants.Replace("  ", " ")
+    $entry.Participants = $entry.Participants.Replace("- ", "-")
+    $entry.emails = $entry.emails.Replace(";", ",")
+    $entry.emails = $entry.emails.Replace(", ", ",")
+    $entry.Participants = $entry.Participants.Replace("<br>", ",")
+    $entry.emails = $entry.emails.Replace(";", ",")
+    $entry.emails = $entry.emails.Replace(", ", ",")
+    $participants = $entry.Participants.Split(',')
+    $emails = $entry.emails.Split(',')
+    $i = 0
+    foreach ($participant in $participants) {
+        if ($emails.Count -gt $i) {
+            if ($emails[$i] -like '*@*') {
+                $names = $participant.Split(" ")
+                if ($names.Count -gt 0) {
+                    $lastname = $names[$names.Count - 1]
+                }
+                # $names[0] + "," + $lastname + "," + $emails[$i]
+                $person = New-Object PSObject
+                $person | Add-member -NotePropertyName "firstName" -NotePropertyValue $names[0]
+                $person | Add-member -NotePropertyName "lastName" -NotePropertyValue $lastname
+                $person | Add-member -NotePropertyName "email" -NotePropertyValue $emails[$i]
+                $persons += $person
+            }
+            if ($skipperonly) {
+                break
+            }
+            $i++
+        } else {
+            break
+        }
+    }
+}
+if ($unique) {
+    $persons = $persons | Sort email -Unique 
+}
+$persons | ConvertTo-Csv -NoTypeInformation
